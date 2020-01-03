5fc21561c70b7e2ce7f7f73e3e40bf53c9f0319f
Christian Neumann <cneumann@students.uni-mainz.de>
2020-01-03 23:38:39 +0100

    init

---
+++ Assets/_Projekt/Scripts/Portal/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera PlayerCamera;
+    public Plane PlayerPlane;
+    public Plane HiddenPlane;
+
+    public Camera thisCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var Rot = Quaternion.LookRotation(-HiddenPlane.Normal) * Quaternion.Inverse(Quaternion.LookRotation(PlayerPlane.Normal));
+
+        var RelPlayerPos = PlayerCamera.transform.localPosition - PlayerPlane.Center;
+        var RelHiddenPos = Rot * RelPlayerPos;
+        transform.localPosition = RelHiddenPos + HiddenPlane.Center;
+        transform.localRotation = Rot * PlayerCamera.transform.localRotation;
+
+        thisCamera.fieldOfView = PlayerCamera.fieldOfView;
+    }
+}
---
+++ Assets/_Projekt/Scripts/Portal/Plane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane: MonoBehaviour
+{
+    public Vector3 Center { get; set; }
+    public Vector3 Normal { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+        Debug.Log(Center);
+        Debug.Log(Normal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Init() {
+        Center = transform.localPosition;
+        Normal = transform.localRotation * new Vector3(0,1,0);
+    }
+}
