16755c55bdd4ed1bb552e2a862a5a62ed6a76791
Patrick Jaberg <patrick>
2020-01-04 21:21:00 +0100

    blobi dev, dead end room

---
+++ Assets/_Projekt/Scripts/Blobi/BlobiMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobiMovement : MonoBehaviour
+{
+    public float maxDistance = 20.0f;
+    public float jumpHeight = 1.0f;
+    public float jumpDistance = 1.0f;
+    public float jumpTimeout = 3.0f;
+
+    private Rigidbody rigidb;
+    private Transform target;
+
+    private float jumpTimer;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rigidb = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        var direction = target.position - transform.position;
+        var xzDirection = new Vector3(direction.x, 0, direction.z).normalized;
+
+        
+        if (direction.magnitude < maxDistance) {
+            jumpTimer -= Time.deltaTime;
+            if (jumpTimer < 0)
+            {
+                rigidb.AddForce(xzDirection * jumpDistance + Vector3.up * jumpHeight);
+                jumpTimer = jumpTimeout;
+            }
+        }
+
+        rigidb.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(-90, 0, 0);
+    }
+}
