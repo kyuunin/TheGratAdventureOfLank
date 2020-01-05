fd17937c03ec81a2944484170c53c3b22120431a
Patrick Jaberg <patrick>
2020-01-05 18:08:57 +0100

    working chests in treasure room

--- Assets/_Projekt/Scripts/CoinScript.cs
+++ Assets/_Projekt/Scripts/CoinScript.cs
@@ -7,6 +7,10 @@ public class CoinScript : MonoBehaviour
     public static int CoinsCollected { get; private set; }
     public static int CoinsSpawned { get; private set; }
 
+    public int value = 1;
+    public bool shouldRotate = true;
+    public GameObject spawnOnCollect = null;
+
     public static int GetRequiredCoinCount()
     {
         float f = CoinsSpawned * 0.80f;
@@ -15,22 +19,26 @@ public class CoinScript : MonoBehaviour
 
     void Awake()
     {
-        CoinsSpawned++;
-        Debug.Log("Coin Spawned!");
+        CoinsSpawned += value;
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            CoinsCollected++;
-            Destroy(gameObject);
+            CoinsCollected += value;
+            if (spawnOnCollect != null)
+                Instantiate(spawnOnCollect, transform.position, Quaternion.Euler(-90,0,0), transform.parent);
+            Destroy(gameObject, 0.1f);
         }
     }
 
     private float angle = 0;
     void Update()
     {
-        angle += Time.deltaTime * 360f * 0.5f;
-        transform.rotation = Quaternion.Euler(0, angle, 0);
+        if(shouldRotate)
+        {
+            angle += Time.deltaTime * 360f * 0.5f;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
     }
 }
