ec8a75e35ff48557c87baa1f49a0b7c6457f19e2
Patrick Jaberg <patrick>
2020-01-06 04:21:36 +0100

    win screen; coins & blobis in all rooms

---
+++ Assets/_Projekt/Scripts/UI/WinScreenDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreenDisplay : MonoBehaviour
+{
+    public Text winText;
+
+    private void Start()
+    {
+        winText.gameObject.SetActive(false);
+    }
+
+    private bool isWon = false;
+    public void Win()
+    {
+        isWon = true;
+        winText.gameObject.SetActive(true);
+
+    }
+
+    private void Update()
+    {
+        if (isWon)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                CoinScript.Reset();
+            }
+        }
+    }
+}
